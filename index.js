fe38e504d46e4505db66e1ae359871a7e02f924f
Sosodope <sonadolkar93@hotmail.com>
2018-09-10 22:22:29 +0700

    project set up and initial files

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const chalk = require("chalk");
-console.log(chalk.blue("Hello ") + chalk.red("World!"));
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+function handleIndexRequest(req, res) {
+  res.render("home");
+}
+
+app.get("/", handleIndexRequest);
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
